7b1c413841423f2bec9b148110f705154a4c8724
juanpabloruiz <pabloruiz1980@gmail.com>
2026-01-30 17:39:31 -0300

    Small changes

--- conn_sample.php
+++ conn_sample.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
 
-// Mostrar errores
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Cambiar datos del servidor
-$conn = new mysqli('localhost', 'pablo', 'Soledad2025.', 'segur');
+$conn = new mysqli('localhost', 'user', 'password', 'db');
 $conn->set_charset('utf8mb4');
